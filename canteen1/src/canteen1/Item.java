421f0f9f3e3d5327765947804be5a3ed7f504403
sanjeev12 <mr.sanjeevjarga@gmail.com>
2017-10-10 13:26:46 +0545

    asdfasdf

---
+++ canteen1/src/canteen1/Item.java
@@ -0,0 +1,58 @@
+package canteen1;
+public class Item {
+
+    private String itmName, itmCode;
+    private int itmRate, itmQuantity;
+
+    Item() {
+    }
+
+    Item(String itmCode, String itmName, int itmRate, int itmQuantity) {
+        this.itmCode = itmCode;
+        this.itmName = itmName;
+        this.itmRate = itmRate;
+        this.itmQuantity = itmQuantity;
+    }
+
+    void setItem(String itmCode, String itmName, int itmRate, int itmQuantity) {
+        this.itmCode = itmCode;
+        this.itmName = itmName;
+        this.itmRate = itmRate;
+        this.itmQuantity = itmQuantity;
+    }
+
+    String getItemCode() {
+        return itmCode;
+    }
+
+    String getItemName() {
+        return itmName;
+    }
+
+    int getItemRate() {
+        return itmRate;
+    }
+
+    int getItemQuantity() {
+        return itmQuantity;
+    }
+}
+
+
+//class order list 
+class OrderList extends Item {
+    private String tableName;
+ OrderList(){};
+
+    public OrderList(String tableName,String itemName,String itemCode,int rate,int itemQuantity){
+        this.tableName=tableName;
+        super.setItem(itemCode, itemName, rate, itemQuantity);
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+    
+            
+ 
+}
