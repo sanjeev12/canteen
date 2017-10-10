b4f24c1123d3a2eafc5d66ac8ae704c00e327a13
sanjeev12 <mr.sanjeevjarga@gmail.com>
2017-10-10 11:49:08 +0545

    asdfasdf

---
+++ canteen1/src/canteen1/DBConnection.java
@@ -0,0 +1,20 @@
+package canteen1;
+
+import java.sql.*;
+public class DBConnection 
+{
+    public Connection conn;
+    public DBConnection()
+    {
+        try 
+        {
+            //Loading MYSQL Database Driver
+            Class.forName("com.mysql.jdbc.Driver"); 
+            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/canteen","root","");
+        } 
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }  
+}
